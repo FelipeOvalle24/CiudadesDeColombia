f5c9b5084924ae0baa0a5c17a8c4143f4de88b40
felipe ovalle <felipeovalle88@gmail.com>
2023-07-09 18:27:05 -0500

    datos de ciudades de colombia

---
+++ ciudadesjs.js
@@ -0,0 +1,244 @@
+//ACA COMIENZA EL CODIGO DE BOGOTA//
+const alturaBogota = document.getElementById("alturaB")
+const poblacionBogota = document.getElementById("poblacionB")
+const temperaturaBogota = document.getElementById("temperaturaB")
+const botonPoblacionB = document.getElementById("boton-poblacion-bogota")
+const botonTemperaturaB = document.getElementById("boton-temperatura-bogota")
+const botonAlturaB = document.getElementById("boton-altura-bogota")
+
+alturaBogota.style.display = "none"
+poblacionBogota.style.display = "none"
+
+botonPoblacionB.addEventListener("click", poblacionB)
+botonTemperaturaB.addEventListener("click", temperaturaB)
+botonAlturaB.addEventListener("click", alturaB)
+
+function poblacionB(){
+    alturaBogota.style.display = "none"
+    poblacionBogota.style.display = "flex"
+    temperaturaBogota.style.display = "none"
+}
+
+function temperaturaB(){
+    alturaBogota.style.display = "none"
+    poblacionBogota.style.display = "none"
+    temperaturaBogota.style.display = "flex"
+}
+
+function alturaB(){
+    alturaBogota.style.display = "flex"
+    poblacionBogota.style.display = "none"
+    temperaturaBogota.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE BOGOTA//
+
+
+//ACA COMIENZA EL CODIGO DE MEDELLIN//
+const alturaMedellin = document.getElementById("alturaM")
+const poblacionMedellin = document.getElementById("poblacionM")
+const temperaturaMedellin = document.getElementById("temperaturaM")
+const botonPoblacionM = document.getElementById("boton-poblacion-medellin")
+const botonTemperaturaM = document.getElementById("boton-temperatura-medellin")
+const botonAlturaM = document.getElementById("boton-altura-medellin")
+
+alturaMedellin.style.display = "none"
+poblacionMedellin.style.display = "none"
+
+botonPoblacionM.addEventListener("click", poblacionM)
+botonTemperaturaM.addEventListener("click", temperaturaM)
+botonAlturaM.addEventListener("click", alturaM)
+
+function poblacionM(){
+    alturaMedellin.style.display = "none"
+    poblacionMedellin.style.display = "flex"
+    temperaturaMedellin.style.display = "none"
+}
+
+function temperaturaM(){
+    alturaMedellin.style.display = "none"
+    poblacionMedellin.style.display = "none"
+    temperaturaMedellin.style.display = "flex"
+}
+
+function alturaM(){
+    alturaMedellin.style.display = "flex"
+    poblacionMedellin.style.display = "none"
+    temperaturaMedellin.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE MEDELLIN//
+
+
+//ACA COMIENZA EL CODIGO DE CARTAGENA//
+const alturaCartagena = document.getElementById("alturaC")
+const poblacionCartagena = document.getElementById("poblacionC")
+const temperaturaCartagena = document.getElementById("temperaturaC")
+const botonPoblacionC = document.getElementById("boton-poblacion-cartagena")
+const botonTemperaturaC = document.getElementById("boton-temperatura-cartagena")
+const botonAlturaC = document.getElementById("boton-altura-cartagena")
+
+alturaCartagena.style.display = "none"
+poblacionCartagena.style.display = "none"
+
+botonPoblacionC.addEventListener("click", poblacionC)
+botonTemperaturaC.addEventListener("click", temperaturaC)
+botonAlturaC.addEventListener("click", alturaC)
+
+function poblacionC(){
+    alturaCartagena.style.display = "none"
+    poblacionCartagena.style.display = "flex"
+    temperaturaCartagena.style.display = "none"
+}
+
+function temperaturaC(){
+    alturaCartagena.style.display = "none"
+    poblacionCartagena.style.display = "none"
+    temperaturaCartagena.style.display = "flex"
+}
+
+function alturaC(){
+    alturaCartagena.style.display = "flex"
+    poblacionCartagena.style.display = "none"
+    temperaturaCartagena.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE CARTAGENA//
+
+
+//ACA COMIENZA EL CODIGO DE CALI//
+const alturaCali = document.getElementById("alturaCA")
+const poblacionCali = document.getElementById("poblacionCA")
+const temperaturaCali = document.getElementById("temperaturaCA")
+const botonPoblacionCA = document.getElementById("boton-poblacion-cali")
+const botonTemperaturaCA = document.getElementById("boton-temperatura-cali")
+const botonAlturaCA = document.getElementById("boton-altura-cali")
+
+alturaCali.style.display = "none"
+poblacionCali.style.display = "none"
+
+botonPoblacionCA.addEventListener("click", poblacionCA)
+botonTemperaturaCA.addEventListener("click", temperaturaCA)
+botonAlturaCA.addEventListener("click", alturaCA)
+
+function poblacionCA(){
+    alturaCali.style.display = "none"
+    poblacionCali.style.display = "flex"
+    temperaturaCali.style.display = "none"
+}
+
+function temperaturaCA(){
+    alturaCali.style.display = "none"
+    poblacionCali.style.display = "none"
+    temperaturaCali.style.display = "flex"
+}
+
+function alturaCA(){
+    alturaCali.style.display = "flex"
+    poblacionCali.style.display = "none"
+    temperaturaCali.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE CALI//
+
+
+//ACA COMIENZA EL CODIGO DE SANTA MARTA//
+const alturaSantaMarta = document.getElementById("alturaS")
+const poblacionSantaMarta = document.getElementById("poblacionS")
+const temperaturaSantaMarta = document.getElementById("temperaturaS")
+const botonPoblacionS = document.getElementById("boton-poblacion-santaMarta")
+const botonTemperaturaS = document.getElementById("boton-temperatura-santaMarta")
+const botonAlturaS = document.getElementById("boton-altura-santaMarta")
+
+alturaSantaMarta.style.display = "none"
+poblacionSantaMarta.style.display = "none"
+
+botonPoblacionS.addEventListener("click", poblacionS)
+botonTemperaturaS.addEventListener("click", temperaturaS)
+botonAlturaS.addEventListener("click", alturaS)
+
+function poblacionS(){
+    alturaSantaMarta.style.display = "none"
+    poblacionSantaMarta.style.display = "flex"
+    temperaturaSantaMarta.style.display = "none"
+}
+
+function temperaturaS(){
+    alturaSantaMarta.style.display = "none"
+    poblacionSantaMarta.style.display = "none"
+    temperaturaSantaMarta.style.display = "flex"
+}
+
+function alturaS(){
+    alturaSantaMarta.style.display = "flex"
+    poblacionSantaMarta.style.display = "none"
+    temperaturaSantaMarta.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE SANTA MARTA//
+
+
+//ACA COMIENZA EL CODIGO DE BARRANQUILLA//
+const alturaBarranquilla = document.getElementById("alturaBA")
+const poblacionBarranquilla = document.getElementById("poblacionBA")
+const temperaturaBarranquilla = document.getElementById("temperaturaBA")
+const botonPoblacionBA = document.getElementById("boton-poblacion-barranquilla")
+const botonTemperaturaBA = document.getElementById("boton-temperatura-barranquilla")
+const botonAlturaBA = document.getElementById("boton-altura-barranquilla")
+
+alturaBarranquilla.style.display = "none"
+poblacionBarranquilla.style.display = "none"
+
+botonPoblacionBA.addEventListener("click", poblacionBA)
+botonTemperaturaBA.addEventListener("click", temperaturaBA)
+botonAlturaBA.addEventListener("click", alturaBA)
+
+function poblacionBA(){
+    alturaBarranquilla.style.display = "none"
+    poblacionBarranquilla.style.display = "flex"
+    temperaturaBarranquilla.style.display = "none"
+}
+
+function temperaturaBA(){
+    alturaBarranquilla.style.display = "none"
+    poblacionBarranquilla.style.display = "none"
+    temperaturaBarranquilla.style.display = "flex"
+}
+
+function alturaBA(){
+    alturaBarranquilla.style.display = "flex"
+    poblacionBarranquilla.style.display = "none"
+    temperaturaBarranquilla.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE BARRANQUILLA//
+
+
+//ACA COMIENZA EL CODIGO DE BUCARAMANGA//
+const alturaBucaramanga = document.getElementById("alturaBU")
+const poblacionBucaramanga = document.getElementById("poblacionBU")
+const temperaturaBucaramanga = document.getElementById("temperaturaBU")
+const botonPoblacionBU = document.getElementById("boton-poblacion-bucaramanga")
+const botonTemperaturaBU = document.getElementById("boton-temperatura-bucaramanga")
+const botonAlturaBU = document.getElementById("boton-altura-bucaramanga")
+
+alturaBucaramanga.style.display = "none"
+poblacionBucaramanga.style.display = "none"
+
+botonPoblacionBU.addEventListener("click", poblacionBU)
+botonTemperaturaBU.addEventListener("click", temperaturaBU)
+botonAlturaBU.addEventListener("click", alturaBU)
+
+function poblacionBU(){
+    alturaBucaramanga.style.display = "none"
+    poblacionBucaramanga.style.display = "flex"
+    temperaturaBucaramanga.style.display = "none"
+}
+
+function temperaturaBU(){
+    alturaBucaramanga.style.display = "none"
+    poblacionBucaramanga.style.display = "none"
+    temperaturaBucaramanga.style.display = "flex"
+}
+
+function alturaBU(){
+    alturaBucaramanga.style.display = "flex"
+    poblacionBucaramanga.style.display = "none"
+    temperaturaBucaramanga.style.display = "none"
+}
+//ACA TERMINA EL CODIGO DE BUCARAMANGA//
+
